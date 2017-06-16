6e63e64852a55c9bb4f51f6e00358d78b55f56c8
Yuan Wang <wangyuan0915@gmail.com>
2017-06-16 16:58:11 -0400

    Lec11

--- examples/Lecture11/beginning-of-lecture-code/part1/app.js
+++ examples/Lecture11/beginning-of-lecture-code/part1/app.js
@@ -7,6 +7,11 @@ angular.module('MsgApp', [])
 MsgController.$inject = ['$scope'];
 function MsgController($scope) {
   $scope.name = "Yaakov";
+  $scope.saveMessage = function () {
+  	// body...
+  	return "Too young Too simple, sometimes naive";
+  }
+
 }
 
 })();
--- examples/Lecture11/beginning-of-lecture-code/part2/app.js
+++ examples/Lecture11/beginning-of-lecture-code/part2/app.js
@@ -8,8 +8,16 @@ MsgController.$inject = ['$scope'];
 function MsgController($scope) {
   $scope.name = "Yaakov";
 
+  $scope.stateOfBeing = "hungry";
+
+
   $scope.sayMessage = function() {
     return "Yaakov likes to eat healthy snacks at night!";
+  }
+
+  $scope.feedYaakov = function () {
+  	// body...
+  	$scope.stateOfBeing = "fed";
   }
 }
 
